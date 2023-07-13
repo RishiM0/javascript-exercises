3eccd0bd54c0a4793c7d3d77a6af23c991ef9172
RishiM0 <rishi.mantri0803@gmail.com>
2023-07-13 18:16:15 -0400

    know which inbuilt methods or functions need to return or how to set default values, reduce for example requires an initial value after the curlybraces and a comma, it also needs you to return the accumulator or else it will be undefined in the next iteration

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach((person) => {
+        if (!person.hasOwnProperty('yearOfDeath')) {
+            person['yearOfDeath'] = new Date().getFullYear();
+        }
+        person['age'] = person['yearOfDeath'] - person['yearOfBirth']
+    })
 
+    let resultIndex = 0;
+    let resultt = people.reduce((longest, person, index) => {
+        if (person['age'] > longest){
+            resultIndex = index;
+            longest = person['age']
+        }
+        return longest;
+    }, 0)
+
+    return people[resultIndex];
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
