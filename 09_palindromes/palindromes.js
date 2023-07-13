c0055086fa71acb312401965d6f5a254bdaa92a0
RishiM0 <rishi.mantri0803@gmail.com>
2023-07-13 16:45:02 -0400

    made palindrome, calculator would've been too easy

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (potential) {
+    potential = potential.toLowerCase();
+    let splitted = potential.split("");
+    let alphabet = 'qwertyuiopasdfghjklzxcvbnm'
+    let result = "";
+    for (let i = 0; i < splitted.length; i++){
+        for (let j in alphabet){
+            if (splitted[i] == alphabet[j]) {
+                result += splitted[i]
+            }
+        }
+    }
 
+    let reversed = "";
+    for (let i = result.length-1; i >= 0; i--){
+        reversed += result[i]
+    }
+    console.log(reversed)
+    console.log(result)
+
+    for (let i = 0; i < result.length; i++){
+            if (result[i] != reversed[i]) {
+                return false;
+            }
+    }
+
+    return true;
+    
 };
+
+console.log(palindromes('r3ace3car'))
 
 // Do not edit below this line
 module.exports = palindromes;
